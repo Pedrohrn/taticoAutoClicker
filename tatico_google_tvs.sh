09e9200f743f7c4ce0a49d3738593b3bcbc46e8c
Pedro Nogueira <pedrohrnogueira@outlook.com>
2026-07-27 00:22:58 -0300

    feat: persistent google restarting and opening

---
+++ tatico_google_tvs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# dando tempo pro sistema ligar componentes de vídeo e afins 
+sleep 30 
+
+# chrome keep-alive
+while true; do
+    # trocar pelo link da padaria/açougue
+    google-chrome --kiosk --incognito --disable-infobars --no-first-run "https://app.powerbi.com/view?r=LINK_AQUI"
+
+    # se ochrome fechar por qualquer motivo, espera 15 segundos e executa novamente
+    sleep 15
+done
+
+# nao esqueça de dar permissao pro script com o comando:
+# chmod +x ~/tatico_google_tvs.sh
