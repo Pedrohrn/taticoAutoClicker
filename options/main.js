359a0fc15a607f3fee65dca5dd78b978a47f4bec
Pedro Nogueira <pedrohrnogueira@outlook.com>
2026-07-31 00:02:34 -0300

    fix imports

--- options/main.js
+++ options/main.js
@@ -1,7 +1,7 @@
-import { initProfiles } from './profiles.js';
-import { initRoutines } from './routines.js';
-import { initRevolver } from './revolver.js';
-import { initStorage } from './storage.js';
+import { initProfiles } from 'options/profiles.js';
+import { initRoutines } from 'options/routines.js';
+import { initRevolver } from 'options/revolver.js';
+import { initStorage } from 'options/storage.js';
 
 // garanto o estado inicial do sistema caso seja a primeira instalacao
 function garantirEstadoInicial() {
