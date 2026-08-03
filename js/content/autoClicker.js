53502021fef56e2d9529d6e47320e7745df22a39
Pedro Nogueira <pedrohrnogueira@outlook.com>
2026-08-03 12:06:27 -0300

    fix: autorefresh timer

--- js/content/autoClicker.js
+++ js/content/autoClicker.js
@@ -197,7 +197,7 @@ async function executarRotinaAvancada(rotina) {
   let qtdeExecutado = 0;
   let refreshTimer = null;
 
-  if (rotina.autorefresh_ativo && (rotina.autorefresh_min > 0 || rotina.autorefresh_seg > 0)) {
+  if (rotina.autorefresh && (rotina.autorefresh_min > 0 || rotina.autorefresh_seg > 0)) {
     const timeMs = ((rotina.autorefresh_min || 0) * 60 + (rotina.autorefresh_seg || 0)) * 1000;
     console.log(`A página será automáticamente recarregada em ${rotina.autorefresh_min}:${rotina.autorefresh_seg}`);
     refreshTimer = setTimeout(() => { location.reload(); }, timeMs);
